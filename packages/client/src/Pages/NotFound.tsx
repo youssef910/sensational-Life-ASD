507f7169508ee4a3c6832ca4ef64c87c906ee0e6
youssef910 <youssef.sadek910@gmail.com>
2022-02-23 22:39:53 +0000

    create notFound page

---
+++ packages/client/src/Pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='bg-slate-50 text-center text-48 mt-60'>
+      Page requested is not Found
+    </div>
+  );
+};
+
+export default NotFound;
