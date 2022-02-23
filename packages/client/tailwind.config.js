1c0d5f52ac74f99d4cebcbfb8a1de65a6a4a387c
youssef910 <youssef.sadek910@gmail.com>
2022-02-23 16:42:58 +0000

    larna project setup frontend react app backend  server node express

---
+++ packages/client/tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/@themesberg/flowbite/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'd-blue': '#1e6dd0',
+        'd-lBlue': '#a4e4db',
+        'd-Black': '#0a2229',
+        'd-ltBg': '#fff8f3',
+        'd-beige': '#f4dfd0',
+      },
+      fontFamily: {
+        roboto: ['Roboto Slab', 'serif'],
+        nexa: ['nexa_demo_light'],
+        ambit_bold: ['ambit_semi_bold'],
+        ambit_italic: ['ambit_semi_bold_italic'],
+      },
+      fontSize: {
+        15: '15px',
+        16: '16px',
+        17: '17px',
+        20: '20px',
+        22: '22px',
+        24: '24px',
+        25: '25px',
+        26: '26px',
+        40: '40px',
+        48: '48px',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ['group-hover'],
+    },
+  },
+  plugins: [],
+};
