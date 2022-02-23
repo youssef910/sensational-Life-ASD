9d2c2fa5fd1598eb94554c83583757de6b547526
youssef910 <youssef.sadek910@gmail.com>
2022-02-23 23:35:08 +0000

    import NavBar to app

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
 import Home from './Pages/Home/Home';
 import NotFound from './Pages/NotFound';
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className='lg:mx-10 bg-black'>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='*' element={<NotFound />} />
