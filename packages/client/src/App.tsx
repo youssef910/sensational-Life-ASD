58382e40fbde48c80b29670fd9d3a71ac2312736
youssef910 <youssef.sadek910@gmail.com>
2022-02-25 23:22:21 +0000

    remove import of app .css

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import './App.css';
 import Footer from './components/footer/Footer';
 import NavBar from './components/navbar/NavBar';
 import Home from './Pages/Home/Home';
 import NotFound from './Pages/NotFound';
-
 const App = () => {
   return (
     <div className='lg:mx-10 bg-black'>
