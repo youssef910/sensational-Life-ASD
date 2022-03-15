57f84f4b7fe49d571c0b6376edada9cbc854bbdf
youssef910 <youssef.sadek910@gmail.com>
2022-03-15 23:45:55 +0000

    add link to about page

--- packages/client/src/Pages/Home/AboutUs.tsx
+++ packages/client/src/Pages/Home/AboutUs.tsx
@@ -27,7 +27,7 @@ const AboutUs = () => {
           professionally and personally.
         </p>
         <div>
-          <Link to='/about'>
+          <Link to='/about-us'>
             <button
               className={` border-1 group focus:ring-d-Black-300 my-5
               ml-6 w-fit bg-d-Black px-4 py-2 text-center font-ambit_bold text-24 text-white hover:bg-stone-600 focus:outline-none focus:ring `}
